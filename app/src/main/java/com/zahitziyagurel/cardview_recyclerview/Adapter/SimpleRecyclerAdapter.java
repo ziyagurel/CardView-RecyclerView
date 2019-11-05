0d36d7fdbc98e32d04a2e6f4b8cb5b6f573a0e35
Zahit Ziya Gurel <ziyagurel55@hotmail.com>
2019-11-05 12:50:42 +0300

    ViewHolder and interface added

--- app/src/main/java/com/zahitziyagurel/cardview_recyclerview/Adapter/SimpleRecyclerAdapter.java
+++ app/src/main/java/com/zahitziyagurel/cardview_recyclerview/Adapter/SimpleRecyclerAdapter.java
@@ -2,11 +2,13 @@ package com.zahitziyagurel.cardview_recyclerview.Adapter;
 
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.zahitziyagurel.cardview_recyclerview.Interface.ICustomItemClickListener;
 import com.zahitziyagurel.cardview_recyclerview.R;
 import com.zahitziyagurel.cardview_recyclerview.Value.person;
 
@@ -30,24 +32,38 @@ public class SimpleRecyclerAdapter extends RecyclerView.Adapter<SimpleRecyclerAd
         }
     }
     List<person> personList;
-    
+    ICustomItemClickListener listener;
+    public SimpleRecyclerAdapter(List<person> personList, ICustomItemClickListener listener){
+        this.listener = listener;
+        this.personList = personList;
+    }
     @Override
     public SimpleRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(SimpleRecyclerAdapter holder, int position) {
-
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_card_view_layout, parent, false);
+        final ViewHolder viewHolder = new ViewHolder(view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onItemClick(view, viewHolder.getPosition());
+            }
+        });
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(SimpleRecyclerAdapter.ViewHolder holder, int position) {
-
+        holder.person_name.setText(personList.get(position).getName());
+        holder.person_age.setText(personList.get(position).getAge());
+        holder.person_img.setImageResource(personList.get(position).getPhoto_id());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return personList.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
     }
 }
