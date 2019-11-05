6f25e45d1ceb6cecffed61e19b05d1409e45695a
Zahit Ziya Gurel <ziyagurel55@hotmail.com>
2019-11-05 12:53:38 +0300

    ViewHolder and interface added

---
+++ app/src/main/java/com/zahitziyagurel/cardview_recyclerview/Interface/ICustomItemClickListener.java
@@ -0,0 +1,7 @@
+package com.zahitziyagurel.cardview_recyclerview.Interface;
+
+import android.view.View;
+
+public interface ICustomItemClickListener {
+    void onItemClick(View v, int possition);
+}
